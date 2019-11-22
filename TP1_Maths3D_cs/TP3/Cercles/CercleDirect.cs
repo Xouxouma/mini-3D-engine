b314dcf5d541f5c5bcce0fac083c5989893ea9a2
grosboisma <grosboisma@eisti.eu>
2019-11-22 17:22:12 +0100

    Conversions entres droits

---
+++ TP1_Maths3D_cs/TP3/Cercles/CercleDirect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moteur3D
+{
+    class CercleDirect
+    {
+        private VectCartesien centre;
+        private VectCartesien rayon;
+
+        public CercleDirect()
+    }
+}
